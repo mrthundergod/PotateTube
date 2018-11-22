9c1ce41036f3afa468c1b45dbe9bcd4524a0532a
Aravind AJ <35934260+mrthundergod@users.noreply.github.com>
2018-11-22 19:20:10 +0530

    Gets Clipboard data and sees if its a youtube link

--- main.py
+++ main.py
@@ -1,26 +1,48 @@
-import youtube-dl
-import win32clipboard as cp
+import youtube_dl
+import tkinter as tk
 import re
-
+import msvcrt as ms
 data= None
 
 
-def startProgram():
-    cp.OpenClipboard()
-    cp.EmptyClipboard()
-
-def endProgram():
-    cp.CloseClipboard()
-
 def urlTest(data):
-    return True
+    testIs=re.match('https://www.youtube.com/',data)
+    return testIs
+
+def getUrlWindow(data):
+    root=tk.Tk()
+    show=tk.Label(root, text="Potato Tube")
+    show1=tk.Label(root, text="Processing!")
+    show.pack()
+    show1.pack()
+   
+    while data == None:
+        data = root.clipboard_get()
+        #ms.getch()
+
+        if urlTest(data) != None:
+            print(data)
+            
+            return data
+        else:
+            #data = None
+            print('try again')
+            continue
+    root.mainloop()
+    return data
+
+def sendtoYDL(data):
+    ydl_opts ={
+        'format': 'bestaudio/best',
+        'postprocessors':[{
+            'key': 'FFmpegExtractAudio',
+            'prefferedcodec': 'mp3',
+            'preferredquality': '192',
+            }]
+        }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([data])
 
 if __name__ == '__main__':
 
-    while data = None:
-        data=cp.GetClipboardData()
-
-        if urlTest(data) == True:
-            endProgram()
-        else:
-            continue
+    data = getUrlWindow(data)
